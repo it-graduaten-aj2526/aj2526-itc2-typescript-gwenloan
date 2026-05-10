eef01ca41e3972b53e3c478f5b729822006ebdea
gwen.matthyssen@gmail.com <Gwen.matthyssen@gmail.com>
2026-05-10 13:53:16 +0200

    vragen worden opgehaald en aangevuld

--- src/pages/QuestionsPage.ts
+++ src/pages/QuestionsPage.ts
@@ -31,69 +31,7 @@ const fillDifficulty = async () => {
     });
 };
 
-// language=HTML
-const apiModeHtml: string = `
-    <h2>API questions</h2>
-    <p>Configure the API for retrieving questions</p>
-    <select class="form-select" id="input-difficulty" data-testid="input-difficulty">${fillDifficulty()}</select>
-    <select class="form-select mt-2" id="input-category" data-testid="input-category">${fillCategories()}</select>
-    <button id="btn-fetch-questions" class="btn btn-primary mt-2" data-testid="btn-fetch-questions">Fetch questions</button>`;
 
-// language=HTML
-const customModeHtml: string = `
-    <h2>Custom questions</h2>
-    <div class="row mb-3">
-        <label for="input-question" class="col-sm-2 col-form-label">Question</label>
-        <div class="col-sm-10">
-            <input class="form-control" id="input-question" data-testid="input-question">
-        </div>
-    </div>
-    <div class="row mb-3">
-        <label for="input-correct-answer" class="col-sm-2 col-form-label">Correct answer</label>
-        <div class="col-sm-10">
-            <input class="form-control" id="input-correct-answer" data-testid="input-correct-answer">
-        </div>
-    </div>
-    <div class="row mb-3">
-        <label for="input-incorrect-answer" class="col-sm-2 col-form-label">Incorrect answer</label>
-        <div class="col-sm-10">
-            <div class="input-group">
-                <input id="input-incorrect-answer" type="text" class="form-control" aria-label="Recipient's username"
-                       aria-describedby="button-addon2" data-testid="input-incorrect-answer">
-                <button class="btn btn-outline-secondary" type="button" id="btn-add-incorrect-answer" data-testid="btn-add-incorrect-answer">Add</button>
-            </div>
-        </div>
-    </div>
-    <table class="table table-bordered">
-        <thead>
-        <tr>
-            <th scope="col">Question</th>
-            <th scope="col">Correct answer</th>
-            <th scope="col">Incorrect answers</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td id="output-question" data-testid="output-question"></td>
-            <td>
-                <ul id="output-correct-answer" data-testid="output-correct-answer">
-                </ul>
-            </td>
-            <td>
-                <ul id="output-incorrect-answers" data-testid="output-incorrect-answers">
-                </ul>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <button type="submit" class="btn btn-primary" id="btn-submit-question" data-testid="btn-submit-question">Submit question</button>
-`;
-
-//language=HTML
-const questionsHtml: string = `
-    <h2 class="mt-2">Confirmed questions <span id="question-counter" data-testid="question-counter">(0/)</span></h2>
-    <div id="questions" data-testid="questions">No questions to display</div>
-`;
 
 export class QuestionsPage {
     private tempQuestion = new Question("");
@@ -103,8 +41,72 @@ export class QuestionsPage {
     }
 
     public async init(contentElement: HTMLElement) {
-        //language=HTML
+
+        // language=HTML
+        const apiModeHtml: string = `
+            <h2>API questions</h2>
+            <p>Configure the API for retrieving questions</p>
+            <select class="form-select" id="input-difficulty" data-testid="input-difficulty">${fillDifficulty()}</select>
+            <select class="form-select mt-2" id="input-category" data-testid="input-category">${fillCategories()}</select>
+            <button id="btn-fetch-questions" class="btn btn-primary mt-2" data-testid="btn-fetch-questions">Fetch questions</button>
+        `;
+
+        const customModeHtml: string = `
+            <h2>Custom questions</h2>
+            <div class="row mb-3">
+                <label for="input-question" class="col-sm-2 col-form-label">Question</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="input-question" data-testid="input-question">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="input-correct-answer" class="col-sm-2 col-form-label">Correct answer</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="input-correct-answer" data-testid="input-correct-answer">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label for="input-incorrect-answer" class="col-sm-2 col-form-label">Incorrect answer</label>
+                <div class="col-sm-10">
+                    <div class="input-group">
+                        <input id="input-incorrect-answer" type="text" class="form-control" aria-label="Recipient's username"
+                            aria-describedby="button-addon2" data-testid="input-incorrect-answer">
+                        <button class="btn btn-outline-secondary" type="button" id="btn-add-incorrect-answer" data-testid="btn-add-incorrect-answer">Add</button>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">Question</th>
+                    <th scope="col">Correct answer</th>
+                    <th scope="col">Incorrect answers</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td id="output-question" data-testid="output-question"></td>
+                    <td>
+                        <ul id="output-correct-answer" data-testid="output-correct-answer">
+                        </ul>
+                    </td>
+                    <td>
+                        <ul id="output-incorrect-answers" data-testid="output-incorrect-answers">
+                        </ul>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <button type="submit" class="btn btn-primary" id="btn-submit-question" data-testid="btn-submit-question">Submit question</button>
+        `;
+
         let htmlToShow = quiz.getQuestionMode() === QuestionMode.Api ? apiModeHtml : customModeHtml;
+        
+        const questionsHtml: string = `
+            <h2 class="mt-2">Confirmed questions <span id="question-counter" data-testid="question-counter">(0/${quiz.quizDuration})</span></h2>
+            <div id="questions" data-testid="questions">No questions to display</div>
+        `;
+
         const fullHtml = `
             <div class="row">
                 <div class="col">
@@ -128,19 +130,20 @@ export class QuestionsPage {
         await fillCategories();
         await fillDifficulty();
 
-    }
-
-    private addQuestions()
-    {
-        let selectedDifficulty: string = "";
-        let selectedCategory: string = "";
+        let selectedDifficulty: string = getElementWrapper<HTMLButtonElement>('#input-difficulty')!.value;
+        let selectedCategory: string = getElementWrapper<HTMLSelectElement>("#input-category")!.value;
 
         getElementWrapper<HTMLButtonElement>('#input-difficulty').addEventListener("change", () => {selectedDifficulty = getElementWrapper<HTMLSelectElement>("#input-difficulty")!.value;});
         getElementWrapper<HTMLButtonElement>('#input-category').addEventListener("change", () => {selectedCategory = getElementWrapper<HTMLSelectElement>("#input-category")!.value;});
     
-        getElementWrapper<HTMLButtonElement>('#btn-fetch-questions').addEventListener("click", () => { questionService.getQuestions(quiz.quizDuration, Number(selectedCategory), selectedDifficulty)});
-        getElementWrapper<HTMLButtonElement>('#btn-fetch-questions').addEventListener("click", () => {console.log(selectedDifficulty,selectedCategory)});
+        getElementWrapper<HTMLButtonElement>('#btn-fetch-questions').addEventListener("click", async () => { 
+            const questions = await questionService.getQuestions(quiz.quizDuration, Number(selectedCategory), selectedDifficulty)
+            const questionsArray = getElementWrapper<HTMLDivElement>('#questions');
+
+            if(questionsArray !== null)
+            {
+                questionsArray.innerHTML = questions!.map(p => `<p>${p.question}</p>`).join('')
+            }
+        });
     }
-
-
 }
